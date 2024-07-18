696cf86bc45e2219a206ba499162f67565ae9694
phamvant <pham.t.286.01@gmail.com>
2024-07-18 10:23:45 +0900

    toggle show all feature post

--- components/HighlightArea.tsx
+++ components/HighlightArea.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { ArrowDown } from "lucide-react";
 
 function HighlightCard({ post }: { post: any }) {
   return (
@@ -11,18 +16,30 @@ function HighlightCard({ post }: { post: any }) {
 }
 
 export default function HighlightArea({ posts }: { posts: any[] }) {
+  const [isShowAll, setShowAll] = useState<boolean>(false);
+
   return (
     <>
       <p className="text-2xl font-bold mb-5">Feature</p>
       <div className="flex flex-col gap-4">
         {posts.map((post, index) => {
           return (
-            <Link href={`/post/${post.id}`} key={index}>
+            <Link
+              href={`/post/${post.id}`}
+              key={index}
+              className={`${!isShowAll && index > 4 ? "hidden" : "static"}`}
+            >
               <HighlightCard post={post} />
             </Link>
           );
         })}
       </div>
+      <Button
+        className="w-full rounded-full mt-5"
+        onClick={() => setShowAll((prev) => !prev)}
+      >
+        {isShowAll ? "Show less" : "Show more"}
+      </Button>
     </>
   );
 }
