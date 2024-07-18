0f9ebb6effa79005f8d6e63b015965f1fec54703
thuan <pham.t.286.01@gmail.com>
2024-07-19 08:12:08 +0900

    feature post animation

--- components/HighlightArea.tsx
+++ components/HighlightArea.tsx
@@ -3,14 +3,12 @@
 import Link from "next/link";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { ArrowDown } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-function HighlightCard({ post }: { post: any }) {
+function HighlightCard({ post, className }: { post: any; className?: string }) {
   return (
-    <div className="rounded-xl p-4 bg-card border border-border flex flex-col gap-4">
+    <div className={cn(`p-4 flex flex-col gap-4 border rounded-xl`, className)}>
       <h1> {post.title}</h1>
-
-      <h2 className="text-sm flex justify-end"> {post.username}</h2>
     </div>
   );
 }
@@ -21,25 +19,36 @@ export default function HighlightArea({ posts }: { posts: any[] }) {
   return (
     <>
       <p className="text-2xl font-bold mb-5">Feature</p>
-      <div className="flex flex-col gap-4">
+      <div className={`flex flex-col gap-4`}>
         {posts.map((post, index) => {
-          return (
-            <Link
-              href={`/post/${post.id}`}
-              key={index}
-              className={`${!isShowAll && index > 4 ? "hidden" : "static"}`}
-            >
-              <HighlightCard post={post} />
-            </Link>
-          );
+          if (index < 4) {
+            return (
+              <Link href={`/post/${post.id}`} key={index}>
+                <HighlightCard post={post} />
+              </Link>
+            );
+          }
         })}
+        <div
+          className={`flex flex-col transition-all gap-4 duration-200 ${
+            isShowAll ? "max-h-0" : "max-h-[999px]"
+          } overflow-hidden`}
+        >
+          {posts.map((post, index) => {
+            if (index > 3) {
+              return (
+                <Link href={`/post/${post.id}`} key={index}>
+                  <HighlightCard post={post} />
+                </Link>
+              );
+            }
+          })}
+        </div>
       </div>
       <Button
-        className="w-full rounded-full mt-5"
+        className="w-full rounded-full mt-4"
         onClick={() => setShowAll((prev) => !prev)}
-      >
-        {isShowAll ? "Show less" : "Show more"}
-      </Button>
+      ></Button>
     </>
   );
 }
