aeddf1d714c8072b360a168876006048947d6368
phamvant <pham.t.286.01@gmail.com>
2024-07-19 17:01:05 +0900

    add loading post page

--- app/(main)/post/[slug]/loading.tsx
+++ app/(main)/post/[slug]/loading.tsx
@@ -1,3 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
-  return <p></p>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Skeleton className="w-2/3 h-10 m-auto mt-10" />
+      <Skeleton className="w-1/3 h-10 m-auto" />
+      <Skeleton className="w-1/3 h-10 m-auto mb-20" />
+      {Array.from({ length: 15 }, (_, index) => (
+        <Skeleton key={index} className="w-4/5 h-10 m-auto" />
+      ))}
+    </div>
+  );
 }
